e2515ae1c4826ced3c9da01443085f187293243e
paOol <paoolkim@gmail.com>
2021-09-10 11:22:57 -0700

    auto format

--- e2e_tests/cchain_nomock.test.ts
+++ e2e_tests/cchain_nomock.test.ts
@@ -8,10 +8,21 @@ describe("CChain", (): void => {
 
   // test_name        response_promise                            resp_fn          matcher           expected_value/obtained_value
   const tests_spec: any = [
-    ["getBaseFee", () => cchain.getBaseFee(), (x) => x, Matcher.toBe, () => "0x34630b8a00"],
-    ["getMaxPriorityFeePerGas", () => cchain.getMaxPriorityFeePerGas(), (x) => x, Matcher.toBe, () => "0x2540be400"],
+    [
+      "getBaseFee",
+      () => cchain.getBaseFee(),
+      (x) => x,
+      Matcher.toBe,
+      () => "0x34630b8a00"
+    ],
+    [
+      "getMaxPriorityFeePerGas",
+      () => cchain.getMaxPriorityFeePerGas(),
+      (x) => x,
+      Matcher.toBe,
+      () => "0x2540be400"
+    ]
   ]
 
   createTests(tests_spec)
-
 })
