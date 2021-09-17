edb3ca264eb73caf521848025833ea581298d968
paOol <paoolkim@gmail.com>
2021-09-17 15:04:40 -0700

    add createUser, and importKey e2e

--- e2e_tests/cchain_nomock.test.ts
+++ e2e_tests/cchain_nomock.test.ts
@@ -6,8 +6,13 @@ import { BlockParameter } from "src/apis/evm/interfaces"
 describe("CChain", (): void => {
   const avalanche = getAvalanche()
   const cchain = avalanche.CChain()
+  const keystore = new KeystoreAPI(avalanche)
 
-  const to: string = "0x197E90f9FAD81970bA7976f33CbD77088E5D7cf7"
+  const user: string = "avalancheJsCChainUser"
+  const passwd: string = "avalancheJsP@ssw4rd"
+  const key: string =
+    "PrivateKey-ewoqjP7PxY4yr3iLTpLisriqt94hdyDFNgchSxGGztUrTXtNN"
+  const to: string = "0x197e90f9fad81970ba7976f33cbd77088e5d7cf7"
   const tag: BlockParameter = "latest"
   const data: string = "0xc92aecc4"
   const hash: string =
@@ -16,8 +21,24 @@ describe("CChain", (): void => {
     "0xf86f028534630b8a0082520894197e90f9fad81970ba7976f33cbd77088e5d7cf7880f43fc2c04ee000080830150f4a0b3b2c1d0348822a4bbfacba46d2525bd3949c8a89a2de015811d2a2a8f695003a07e9ee1c98bfe6e35cce932540c25f2552fb67f3a22c5fdd51b63d7e35363b9e2"
   const txHash: string =
     "0x58cbd8363e9bf3258779599f482f914e6646c8bee63b6f65c311d134f547f3f2"
+  const whaleAddr: string = "0x8db97C7cEcE249c2b98bDC0226Cc4C2A57BF52FC"
+
   // test_name        response_promise                            resp_fn          matcher           expected_value/obtained_value
   const tests_spec: any = [
+    [
+      "createUser",
+      () => keystore.createUser(user, passwd),
+      (x) => x,
+      Matcher.toBe,
+      () => true
+    ],
+    [
+      "importKey",
+      () => cchain.importKey(user, passwd, key),
+      (x) => x,
+      Matcher.toBe,
+      () => whaleAddr
+    ],
     [
       "getBaseFee",
       () => cchain.getBaseFee(),
