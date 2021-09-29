c76b8a4176200095b693c82a82d76bfaa86abc13
paOol <paoolkim@gmail.com>
2021-09-29 16:15:12 -0700

    index x-chain transactions

--- e2e_tests/index_nomock.test.ts
+++ e2e_tests/index_nomock.test.ts
@@ -19,66 +19,99 @@ describe("Index", () => {
   const key: string =
     "PrivateKey-ewoqjP7PxY4yr3iLTpLisriqt94hdyDFNgchSxGGztUrTXtNN"
 
-  const txBytes: string =
-    "11111111JvKptXTaQ2uHBi6XcCA7ZzcsegZxXU9tcjx16oorLZYvar6DEstkNaKtqvTuLvCrAM8rySf5rKGqMXwb9kvaKSeruScX1kZXbR5kuUAKSDxi1sihZi4fSh6v7QPxAYgjrHp3BXxCQ9cCPUkdUi5xezJqpLFZyCJjF27xC6mWbHZTuyHtrTYCBYfSE59z6A4LvHPh77T2QJTieAFiiGxJto7PLwmJVKrK5DpEGAHvzz4ThPoS1P6eLhzHDsZBZnqu6jqJ6cf6QL9DJFB4sKQ3yZzAhu6jv7YPDBaVxQnJAGVKeopPCTVddFDp9TzMCprfGEws5dfFDjTrwTA5qdaovCdTma7ewENLeMRm8CqMaQuRfrC2yjXvPsQ61ZrTzibcypVVt8z8myhLvYwxxE6eszmscTLs3s3EaWUJK1VVi4qKz2PHhqzEKXgKe3PgQsf6mXn7HBAgCqWFs3Nk1LeDkxFgmkV2MaiB81V1Yad89wmo88kih9L9V5QnGrqx8cQK"
   const encoding: string = "cb58"
   const baseurl: string = "/ext/index/X/tx"
-  const wait = (waitTime: number) => {
-    return new Promise<void>((resolve) => {
-      setTimeout(() => resolve(), waitTime)
-    })
-  }
+  const cContainerID: string =
+    "2ceDnmxh59AsXqTG95vf3dr2a7ohXprNn9mvWgQJ39uHryBecT"
+
+  const containerID: string =
+    "2ceDnmxh59AsXqTG95vf3dr2a7ohXprNn9mvWgQJ39uHryBecT"
 
   // test_name          response_promise               resp_fn                 matcher           expected_value/obtained_value
   const tests_spec: any = [
+    // [
+    //   "createUser",
+    //   () => keystore.createUser(user, passwd),
+    //   (x) => x,
+    //   Matcher.toBe,
+    //   () => true
+    // ],
+    // [
+    //   "importKey",
+    //   () => xchain.importKey(user, passwd, key),
+    //   (x) => x,
+    //   Matcher.toBe,
+    //   () => whaleAddr
+    // ],
+    // [
+    //   "createaddrB",
+    //   () => xchain.createAddress(user, passwd),
+    //   (x) => x,
+    //   Matcher.Get,
+    //   () => addrB
+    // ],
+    // [
+    //   "send",
+    //   () =>
+    //     xchain.send(
+    //       user,
+    //       passwd,
+    //       "AVAX",
+    //       10,
+    //       addrB.value,
+    //       [whaleAddr],
+    //       whaleAddr,
+    //       "MEMO"
+    //     ),
+    //   (x) => x.txID,
+    //   Matcher.Get,
+    //   () => tx
+    // ],
     [
-      "createUser",
-      () => keystore.createUser(user, passwd),
-      (x) => x,
-      Matcher.toBe,
-      () => true
-    ],
-    [
-      "importKey",
-      () => xchain.importKey(user, passwd, key),
-      (x) => x,
-      Matcher.toBe,
-      () => whaleAddr
-    ],
-    [
-      "createaddrB",
-      () => xchain.createAddress(user, passwd),
-      (x) => x,
-      Matcher.Get,
-      () => addrB
-    ],
-    [
-      "send",
-      () =>
-        xchain.send(
-          user,
-          passwd,
-          "AVAX",
-          10,
-          addrB.value,
-          [whaleAddr],
-          whaleAddr,
-          "MEMO"
-        ),
-      (x) => x.txID,
-      Matcher.Get,
-      () => tx
-    ],
-    [
-      "getLastAccepted",
+      "getXLastAccepted",
       async () => {
         await Index.getLastAccepted(encoding, baseurl)
       },
-      (x) => {
-        return x
+      (x) => x,
+      Matcher.toThrow,
+      () => "no containers have been accepted"
+    ],
+
+    [
+      "getXContainerByIndex",
+      async () => {
+        await Index.getContainerByIndex("0", encoding, "/ext/index/X/tx")
       },
+      (x) => x,
+      Matcher.toThrow,
+      () => "no container at index 0"
+    ],
+    [
+      "getXContainerRange",
+      async () => {
+        await Index.getContainerRange(0, 100, "hex", "/ext/index/X/tx")
+      },
+      (x) => x,
+      Matcher.toThrow,
+      () => "no containers have been accepted"
+    ],
+    [
+      "getXIndex",
+      async () => {
+        Index.getIndex(cContainerID, "hex", "ext/index/X/tx")
+      },
+      (x) => x,
       Matcher.toBe,
-      () => txBytes
+      () => undefined
+    ],
+    [
+      "getXisAccepted",
+      async () => {
+        Index.isAccepted(cContainerID, "hex", "ext/index/X/tx")
+      },
+      (x) => x,
+      Matcher.toBe,
+      () => undefined
     ]
   ]
 
